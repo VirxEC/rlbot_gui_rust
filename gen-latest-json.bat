3f23d1c9b198f30b98afeb59eaa7add510e3a2f5
Eric Veilleux <virx@virxcase.dev>
2022-06-29 11:51:59 -0400

    Update gen-lastest-json.bat

--- gen-latest-json.bat
+++ gen-latest-json.bat
@@ -3,22 +3,19 @@ setlocal enabledelayedexpansion
 
 echo %1
 echo %2
+echo %3
 
-set var1=%1
-set var1=!var1:~1!
-echo !var1!
-
-set /p sig=<src-tauri\target\release\bundle\msi\RLBotGUI_%var1%_x64_en-US.msi.zip.sig
+set /p sig=<src-tauri\target\release\bundle\msi\RLBotGUI_%2_x64_en-US.msi.zip.sig
 echo %sig%
 
 (
 echo { 
 echo   "name": "%1",
-echo   "notes": %2,
+echo   "notes": %3,
 echo   "platforms": {
 echo     "windows-x86_64": {
 echo       "signature": "%sig%",
-echo       "url":"https://github.com/VirxEC/rlbot_gui_rust/releases/download/%1/RLBotGUI_!%var1%_x64_en-US.msi.zip"
+echo       "url":"https://github.com/VirxEC/rlbot_gui_rust/releases/download/%1/RLBotGUI_!%2_x64_en-US.msi.zip"
 echo     }
 echo   }
 echo }
