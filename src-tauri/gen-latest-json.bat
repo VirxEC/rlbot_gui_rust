b3eeaea3373fb0425232a2a2c788e46c0de788e8
VirxEC <virx@virxcase.dev>
2022-06-28 21:37:12 -0400

    Directly read file signature

--- src-tauri/gen-latest-json.bat
+++ src-tauri/gen-latest-json.bat
@@ -5,20 +5,24 @@ rem Version, e.x. v1.0.0
 echo %1
 rem Release notes, e.x. "my release notes"
 echo %2
-rem File signature, e.x. "w97etgho4gbv="
-echo %3
 
+rem Version with first character trimmed - so v1.0.0 becomes 1.0.0
 set var1=%1
 set var1=!var1:~1!
 echo !var1!
 
+rem Get the file signature
+set /p sig=<target\release\bundle\msi\RLBotGUI_%var1%_x64_en-US.msi.zip.sig
+echo %sig%
+
+rem Generate the file
 (
 echo { 
 echo   "name": "%1",
 echo   "notes": %2,
 echo   "platforms": {
 echo     "windows-x86_64": {
-echo       "signature": %3,
+echo       "signature": "%sig%",
 echo       "url":"https://github.com/VirxEC/rlbot_gui_rust/releases/download/%1/RLBotGUI_!%var1%_x64_en-US.msi.zip"
 echo     }
 echo   }
