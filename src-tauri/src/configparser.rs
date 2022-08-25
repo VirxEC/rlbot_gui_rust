8f14b1763fb8a031ef290f98127d96dd9b8ecbc9
Eric Veilleux <virx@virxcase.dev>
2022-08-25 13:11:35 -0400

    Better implement load_async

--- src-tauri/src/configparser.rs
+++ src-tauri/src/configparser.rs
@@ -11,7 +11,7 @@
 
 use indexmap::IndexMap as Map;
 use std::{collections::HashMap, convert::AsRef, fmt::Write, fs, fs::File, io::prelude::*, path::Path};
-use tokio::{fs as async_fs, fs::File as AsyncFile, io::AsyncReadExt};
+use tokio::fs as async_fs;
 
 ///The `Ini` struct simply contains a nested hashmap of the loaded configuration, the default section header and comment symbols.
 ///## Example
@@ -859,23 +859,14 @@ impl Ini {
     ///
     ///Returns `Ok(map)` with a clone of the stored `Map` if no errors are thrown or else `Err(error_string)`.
     ///Use `get_mut_map()` if you want a mutable reference.
-    pub async fn load_async<T: AsRef<Path>>(&mut self, path: T) -> Result<Map<String, Map<String, Option<String>>>, String> {
-        let path = path.as_ref();
-        let display = path.display();
-
-        let mut file = match AsyncFile::open(&path).await {
-            Err(why) => return Err(format!("couldn't open {}: {}", display, why)),
-            Ok(file) => file,
-        };
-
-        let mut s = String::new();
-        self.map = match file.read_to_string(&mut s).await {
-            Err(why) => return Err(format!("couldn't read {}: {}", display, why)),
-            Ok(_) => match self.parse(s) {
-                Err(why) => return Err(why),
-                Ok(map) => map,
-            },
-        };
+    pub async fn load_async<T: AsRef<Path>>(
+        &mut self,
+        path: T,
+    ) -> Result<Map<String, Map<String, Option<String>>>, String> {
+        let s = async_fs::read_to_string(&path)
+            .await
+            .map_err(|why| format!("couldn't read {}: {}", path.as_ref().display(), why))?;
+        self.map = self.parse(s)?;
         Ok(self.map.clone())
     }
 
